592d4c66e569b3734212b99e8f65a00d42f9873b
App Academy Student <student@appacademy.io>
2013-09-23 13:58:21 -0700

    Add find_tile_neighbors method to Board.

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,14 +3,15 @@ class Minesweeper
 
 
   class Board
-    attr_accessor :tiles
+    attr_accessor :tiles, :size
 
     def initialize(size)
       # @tiles = Array.new(size) { Array.new(size, Tile.new) }
+      @size = size
       @tiles = []
-      size.times do |row|
+      @size.times do |row|
         @tiles[row] = []
-        size.times do |column|
+        @size.times do |column|
           @tiles[row][column] << Tile.new(self, row, column)
         end
       end
@@ -18,7 +19,26 @@ class Minesweeper
     end
 
     def find_tile_neighbors(tile)
+      tile_neighbors = []
 
+      neighbor_locations = [[-1, -1], [-1, 0], [-1, 1],
+                            [0, -1],           [0, 1],
+                            [1, -1], [1, 0], [1, 1]]
+      neighbor_locations.each do |location|
+        d_row = tile.position[0] + location[0]
+        d_column = tile.position[1] + location[1]
+
+        if is_on_board?([d_row, d_column])
+          tile_neighbors << @board[d_row][d_column]
+        end
+      end
+
+      tile_neighbors
+    end
+
+    def is_on_board?(position)
+      valid_range = (0...@size)
+      valid_range.include?(position[0]) && valid_range.include?(position[1])
     end
 
   end
