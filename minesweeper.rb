776ac4f86c8467bebf876100dd1b947ad1f1e174
App Academy Student <student@appacademy.io>
2013-09-23 17:04:04 -0700

    Add save game functionality.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,14 +1,23 @@
 require './tile.rb'
 require './board.rb'
+require 'yaml'
 
 class Minesweeper
-  attr_accessor :board, :size
+  INPUTS = {"S" :save_game, "F" :flag_tile, "E" :explore_tile}
+
+  attr_accessor :board
 
   def initialize(size)
-    @board = Board.new(size)
+    @board = Board.new(size = 9)
   end
 
   def run
+    # minesweeper = new_game_prompt
+    # if minesweeper
+    #   minesweeper.run
+    #   return
+    # end
+
     first_move = true
     game_over = false
     start_time = Time.now
@@ -16,21 +25,23 @@ class Minesweeper
     until game_over
       display_game_state
 
-      flagging, position = *get_input
+      input, argument = *get_input
 
       if first_move
         @board.generate_mines(position)
         first_move = false
       end
 
-      explored_mine = process_input(flagging, position)
+      input_result = send(input, argument)
+      return if input == :save_game
+
+      explored_mine = input_result
 
       if explored_mine
         puts @board.render(true)
         print "Mine clicked! You lost"
         game_over = true
       end
-
 
       if @board.cleared?
         print "You won"
@@ -41,28 +52,41 @@ class Minesweeper
     puts " in #{(Time.now - start_time).to_i} seconds"
   end
 
-  def get_input
-    print "Would you like to flag or explore a tile? [F, E] "
-    flagging = gets.chomp.upcase == "F"
-    print "Where would you like to do this? [row, column] "
-
-    location = gets.chomp.split(",").map { |coordinate| coordinate.strip.to_i }
-    tile_position = [location[0] - 1, location[1] - 1]
-    [flagging, tile_position]
-  end
-
   def display_game_state
     puts @board
     puts "Flagged #{@board.count_is_flagged} out of #{@board.mines} mines"
   end
 
-  def process_input(flagging, position)
-    if flagging
-      @board.flag(position)
-      return false
+  def get_input
+    print "Would you like to save or flag or explore a tile? [S, F, E] "
+    input = gets.chomp.upcase
+
+    argument = nil
+    if input == "S"
+      print "What name would you like to save under? "
+      argument = gets.strip
     else
-      @board.explore(position)
+      print "Where would you like to do this? [row, column] "
+      location = gets.split(",").map { |coordinate| coordinate.strip.to_i }
+      argument = [location[0] - 1, location[1] - 1]
     end
+
+    [INPUTS[input], argument]
+  end
+
+  def save_game(name)
+    file = File.open("#{name}", "w")
+    file.write(self.to_yaml)
+    file.close
+  end
+
+  def flag_tile(position)
+    @board.flag(position)
+    false
+  end
+
+  def explore_tile(position)
+    @board.explore(position)
   end
 end
 
